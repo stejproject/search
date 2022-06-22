303cd91a7b5613040eb5ed5957dc3b76f2cadc05
stejproject <106367163+stejproject@users.noreply.github.com>
2022-06-22 16:37:19 +0700

    Add files via upload

---
+++ search.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, h5, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    a = div.getElementsByTagName("h5");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
